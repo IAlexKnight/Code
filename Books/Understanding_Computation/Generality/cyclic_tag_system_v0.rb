3068dc27b619717bf467ae3726b045911fe42c07
IAlexKnight <alexmirageknight@gmail.com>
2017-07-17 23:51:11 +0800

    good part

--- Books/Understanding_Computation/Generality/cyclic_tag_system_v0.rb
+++ Books/Understanding_Computation/Generality/cyclic_tag_system_v0.rb
@@ -1,3 +1,89 @@
+
+class TagRule < Struct.new(:first_character, :append_characters)
+    def applies_to?(string)
+        string.chars.first == first_character
+    end
+
+    def follow(string)
+        string + append_characters
+    end
+
+    def alphabet
+        ([first_character] + append_characters.chars.entries).uniq
+    end
+
+    def to_cyclic(encoder)
+        CyclicTagRule.new(encoder.encode_string(append_characters))
+    end
+end
+
+class TagRulebook < Struct.new(:deletion_number, :rules)
+    def next_string(string)
+        rule_for(string).follow(string).slice(deletion_number..-1)
+    end
+
+    def rule_for(string)
+        rules.detect { |r| r.applies_to?(string) }
+    end
+
+    def applies_to?(string)
+        !rule_for(string).nil? && string.length >= deletion_number
+    end
+
+    def alphabet
+        rules.flat_map(&:alphabet).uniq
+    end
+
+    def cyclic_padding_rules(encoder)
+        Array.new(encoder.alphabet.length, CyclicTagRule.new('')) * (deletion_number - 1)
+    end 
+
+    def to_cyclic(encoder)
+        CyclicTagRulebook.new(cyclic_rules(encoder) + cyclic_padding_rules(encoder))
+    end
+
+    def cyclic_rules(encoder)
+        encoder.alphabet.map { |character| cyclic_rule_for(character,encoder) }
+    end
+
+    def cyclic_rule_for(character, encoder)
+        rule = rule_for(character)
+        
+        if rule.nil?
+            CyclicTagRule.new('')
+        else
+            rule.to_cyclic(encoder)
+        end
+    end
+end
+
+class TagSystem < Struct.new(:current_string, :rulebook)
+    def step
+        self.current_string = rulebook.next_string(current_string)
+    end
+
+    def run
+        while rulebook.applies_to?(current_string)
+            puts current_string
+            step
+        end
+         
+        puts current_string
+    end
+
+    def alphabet
+        (rulebook.alphabet + current_string.chars.entries).uniq.sort
+    end
+
+    def encoder
+        CyclicTagEncoder.new(alphabet)
+    end
+
+    def to_cyclic
+        TagSystem.new(encoder.encode_string(current_string), rulebook.to_cyclic(encoder))
+    end
+end
+
 class CyclicTagRule < TagRule
     FIRST_CHARACTER = '1'
 
@@ -36,32 +122,29 @@ class CyclicTagRulebook < Struct.new(:rules)
     end
 end
 
-rulebook = CyclicTagRulebook.new([
-    CyclicTagRule.new('1'), CyclicTagRule.new('0010'), CyclicTagRule.new('10')
-])
+class CyclicTagEncoder < Struct.new(:alphabet)
+    def encode_string(string)
+        string.chars.map { |character| encode_character(character) }.join
+    end
 
-system = TagSystem.new('11', rulebook)
-16.times do 
-    puts system.current_string
-    system.step
-end; puts system.current_string
+    def encode_character(character)
+        character_position = alphabet.index(character)
+        (0...alphabet.length).map { |n| n == character_position ? '1' : '0' }.join
+    end
+end
 
+#rulebook = CyclicTagRulebook.new([
+#    CyclicTagRule.new('1'), CyclicTagRule.new('0010'), CyclicTagRule.new('10')
+#])
 
-
-
-
-
-
-
-
-
-
-
-
-
+rulebook = TagRulebook.new(2, [TagRule.new('a', 'ccdd'), TagRule.new('b', 'dd')])
+system = TagSystem.new('aabbbbb', rulebook)
+#16.times do 
+#    puts system.current_string
+#    system.step
+#end; puts system.current_string
 
 
-
-
-
+cyclic_system = system.to_cyclic
+cyclic_system.run
 
